6ee2615aaabf142caf1c38338e6613ff83818156
Clement <ckoomson75@gmail.com>
2021-09-18 11:23:18 +0000

    Added authentication UI

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,17 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index');
 });
+
+
+router.get('/login', function(req, res, next) {
+  res.render('auth/login');
+});
+
+router.get('/register', function(req, res, next) {
+  res.render('auth/register');
+});
+
 
 module.exports = router;
